b8a11b280947d53b484823d24d0b41c6cedd8371
Valerio Farina <valerio.farina.1994@gmail.com>
2021-02-05 15:20:58 +0100

    Definito component CustomTitle

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <CustomTitle titleText="Pokemon" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import CustomTitle from "./components/CustomTitle.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    CustomTitle
   }
 };
 </script>
