ede23d5a9a59fe607b1baf899e940bec124a5b5d
Valerio Farina <valerio.farina.1994@gmail.com>
2021-02-05 16:01:07 +0100

    Stampata una card per ogni pokemon

--- src/pages/Pokemons.vue
+++ src/pages/Pokemons.vue
@@ -1,6 +1,19 @@
 <template>
-    <div>
-
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-xs-6 col-md-4 col-lg-3" v-for="(pokemon, index) in pokemons" :key="index">
+                <div class="card mt-2 mb-2">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            {{ pokemon.name }}
+                        </h5>
+                        <p class="card-text">
+                            {{ pokemon.url }}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
